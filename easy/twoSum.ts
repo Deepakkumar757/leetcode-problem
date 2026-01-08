b4f0e2b95285b47e5230e59f1f510e92d54d16f0
Deepakkumar-Dte <deepak@dteagritech.com>
2026-01-08 18:47:09 +0530

    Two sum easy problem solved

---
+++ easy/twoSum.ts
@@ -0,0 +1,17 @@
+/*Two Sum*/
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+function twoSum(nums: number[], target: number): number[] {
+    const numMap = new Map();
+    for(let i=0;i<nums.length;i++){
+        const remain = target-nums[i];
+        if(numMap.has(remain)){
+            return [numMap.get(remain),i];
+        }
+        numMap.set(nums[i],i);
+    }
+    return [];
+};
