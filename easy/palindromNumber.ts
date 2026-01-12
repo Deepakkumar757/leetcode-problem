89dc98434f81f415d3ee6dbe73e43a95ecbc55b1
Deepakkumar0757 <deepakumar0220@gmail.com>
2026-01-12 20:58:57 +0530

    formated the code

--- easy/palindromNumber.ts
+++ easy/palindromNumber.ts
@@ -4,45 +4,44 @@ Given an integer x, return true if x is a palindrome, and false otherwise.
 **/
 
 function isPalindromeConvertedString(x: number): boolean {
-    if(x<0) return false;
-    let str = x.toString();
-    let left = 0;
-    let right = str.length - 1;
-    while(left < right){
-        if(str[left] !== str[right]){
-            return false;
-        }
-        left++;
-        right--;
+  if (x < 0) return false;
+  let str = x.toString();
+  let left = 0;
+  let right = str.length - 1;
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false;
     }
-    return true;
+    left++;
+    right--;
+  }
+  return true;
 }
 
-
 function isPalindromeUsingNumber(x: number): boolean {
-       if (x < 0 || (x % 10 === 0 && x !== 0)) {
-        return false;
-    }
+  if (x < 0 || (x % 10 === 0 && x !== 0)) {
+    return false;
+  }
   let original = x;
   let reversed = 0;
-  
-  while(x>0){
-    const lastDigit = x%10;
-    reversed = (reversed*10) + lastDigit;
-    x = Math.floor(x/10)
+
+  while (x > 0) {
+    const lastDigit = x % 10;
+    reversed = reversed * 10 + lastDigit;
+    x = Math.floor(x / 10);
   }
-    return original === reversed;
-};
+  return original === reversed;
+}
 
 function isPalindromeHalfReversal(x: number): boolean {
-    if (x < 0 || (x % 10 === 0 && x !== 0)) {
-        return false;
-    }
-    let reversedHalf = 0;
-    while (x > reversedHalf) {
-        const lastDigit = x % 10;
-        reversedHalf = (reversedHalf * 10) + lastDigit;
-        x = Math.floor(x / 10);
-    }
-    return x === reversedHalf || x === Math.floor(reversedHalf / 10);
-};
+  if (x < 0 || (x % 10 === 0 && x !== 0)) {
+    return false;
+  }
+  let reversedHalf = 0;
+  while (x > reversedHalf) {
+    const lastDigit = x % 10;
+    reversedHalf = reversedHalf * 10 + lastDigit;
+    x = Math.floor(x / 10);
+  }
+  return x === reversedHalf || x === Math.floor(reversedHalf / 10);
+}
